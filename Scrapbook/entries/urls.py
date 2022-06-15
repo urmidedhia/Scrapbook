1aae795db6f7864f7a48fdad5e9e13cd02e1f70a
Urmi <dedhia.urmi20@gmail.com>
2022-06-15 22:52:30 +0530

    route changes

--- Scrapbook/entries/urls.py
+++ Scrapbook/entries/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from entries import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('thor/', views.index, name='index'),
     path('entry/', views.entry, name='entry'),
-    path('close/', views.close, name='close'),
+    path('hel/', views.close, name='close'),
     path('wall/', views.wall, name='wall'),
     path('family/', views.family, name='family'),
     path('scar/', views.scar, name='scar'),
--- Scrapbook/home/urls.py
+++ Scrapbook/home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('odin/', views.home, name='home'),
 ]
